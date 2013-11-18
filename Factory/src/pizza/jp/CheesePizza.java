fc7dcd6599522892af2924f8739f3ef014587322
kbc11a04 <kbc11a04@kbc11a04-PC>
2013-11-18 13:32:20 +0900

    factory

--- Factory/src/pizza/jp/CheesePizza.java
+++ Factory/src/pizza/jp/CheesePizza.java
@@ -1,5 +1,11 @@
 package pizza.jp;
 
 public class CheesePizza extends Pizza {
+	public CheesePizza() {
+		this.setName("チーズピザ");
+		dough = "薄いクラスト生地";
+		sauce = "マリナラソース";
 
+		toppings.add("粉チーズ");
+	}
 }
